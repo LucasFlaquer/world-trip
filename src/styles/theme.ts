8335529c7ef6a010d32c1cd8714a97ce4ebfe617
LucasFlaquer <lucas.flaquer@gmail.com>
2021-05-25 00:05:00 -0300

    theme setup

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme, Theme } from "@chakra-ui/react";
+
+
+export const theme = extendTheme<Theme>({
+  colors: {},
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  }
+
+})
