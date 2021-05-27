bc12320a82d7d25de157223435c6d5aabd499c9c
LucasFlaquer <lucas.flaquer@gmail.com>
2021-05-27 00:41:14 -0300

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps } from "next/dist/next-server/lib/router/router"
+import { theme } from "../styles/theme"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
