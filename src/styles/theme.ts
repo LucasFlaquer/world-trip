eaa86035fbbf39cc1352f09008981a30811b98b2
LucasFlaquer <lucas.flaquer@gmail.com>
2021-05-27 00:42:29 -0300

    theme updated

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,10 +2,23 @@ import { extendTheme, Theme } from "@chakra-ui/react";
 
 
 export const theme = extendTheme<Theme>({
-  colors: {},
+  colors: {
+    gray: {
+      // "900": 'cyan'
+    },
+    white: '#F5F8FA'
+  },
   fonts: {
     heading: 'Poppins',
     body: 'Poppins'
+  },
+  styles: {
+    global: {
+      '.chakra-divider': {
+        borderColor: '#47585B !important',
+        opacity: 1,
+      },
+    }
   }
 
 })
